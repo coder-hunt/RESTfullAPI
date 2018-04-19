5392e8ab040bf82c42a6afb190ed100f58b3d948
pushpendra-jaypore <pushpendra.kumar@jaypore.com>
2018-04-19 14:49:08 +0530

    balanced update attempts

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# RESTfullAPI
+# SchoolCom Second round conding task 
+  [nodejs]: https://docs.npmjs.com/getting-started/installing-node
+  [mlab]: http://docs.mlab.com/ops/
+
+**Quick start guide list:**
+
+  * [Install NodeJs][nodejs]
+  * [Create mlab account for mongoDB][mlab]
+  * Install jsonwebtoken and bcrypt module 
+     * npm install jsonwebtoken
+     * npm install bcrypt
+     
+ 
+**API check list**
+  
+  * http://localhost:3000/userlist 
+    
+
+
+  
--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var express = require('express'),
   app = express(),
   port = process.env.PORT || 3000,
   mongoose = require('mongoose'),
-  Task = require('./api/models/todoListModel'),
   User = require('./api/models/userModel'),
   bodyParser = require('body-parser');
   jsonwebtoken = require("jsonwebtoken");
@@ -11,12 +10,11 @@ mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://school:school@ds249079.mlab.com:49079/schoolcom');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+//Middleware for user validation start...
 app.use(function(req, res, next){
-
   if(req.headers && req.headers.authorization && req.headers.authorization.split(' ')[0]=='JWT'){
     jsonwebtoken.verify(req.headers.authorization.split(' ')[1], 'schoolcom',function(err, decode){
       if(err){
-        console.log(err);
         req.user = undefined;
       }
       req.user = decode;
@@ -27,11 +25,9 @@ app.use(function(req, res, next){
     next();
   };
 });
-var routes = require('./api/routes/todoListRoutes');
+//Middleware for user validation end.....
+var routes = require('./api/routes/schoolcomRoutes');
 routes(app);
-
-
 app.listen(port);
-
-console.log('todo list RESTful API server started on: ' + port);
+console.log('SchoolCom API server started on: ' + port);
 
