ebabc3130fe895f0827f3f1196e1844cb251cc97
pushpendra-jaypore <pushpendra.kumar@jaypore.com>
2018-04-19 05:45:43 +0530

    Schoolcom assigment with restAPI

--- api/routes/todoListRoutes.js
+++ api/routes/todoListRoutes.js
@@ -1,16 +1,24 @@
 
-module.exports = function(app) {
-  var todoList = require('../controllers/todoListController');
+module.exports = function (app) {
 
+  var userHandlers = require('../controllers/userController.js');
+  
+  app.route('/userlist')
+     .get(userHandlers.loginRequired, userHandlers.getCustomerList);
+  app.route('/createUser') // Create user from database 
+     .put(userHandlers.loginRequired, userHandlers.createUser);
+  app.route('/getUserDetails') // Read user's details from database 
+     .get(userHandlers.loginRequired, userHandlers.getUserDetails);
+  app.route('/updateUser') // update existing user's details from database  
+     .post(userHandlers.loginRequired, userHandlers.updateUser);
+  app.route('/deleteUser') // delete user's details from database 
+     .delete(userHandlers.loginRequired, userHandlers.deleteUser);
+  app.route('/auth/login')  // Login user
+     .post(userHandlers.login);
+  app.route('/auth/register') // Register user 
+     .put(userHandlers.register);
 
-  // todoList Routes
-  app.route('/tasks')
-    .get(todoList.list_all_tasks)
-    .post(todoList.create_a_task);
+  app.route('/balanced')
+     .post(userHandlers.loginRequired,userHandlers.balanced);
 
-
-  app.route('/tasks/:taskId')
-    .get(todoList.read_a_task)
-    .put(todoList.update_a_task)
-    .delete(todoList.delete_a_task);
 };
--- server.js
+++ server.js
@@ -3,15 +3,30 @@ var express = require('express'),
   port = process.env.PORT || 3000,
   mongoose = require('mongoose'),
   Task = require('./api/models/todoListModel'),
+  User = require('./api/models/userModel'),
   bodyParser = require('body-parser');
-  
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://127.0.0.1:27017/Tododb');
+  jsonwebtoken = require("jsonwebtoken");
 
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://school:school@ds249079.mlab.com:49079/schoolcom');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(function(req, res, next){
 
-
+  if(req.headers && req.headers.authorization && req.headers.authorization.split(' ')[0]=='JWT'){
+    jsonwebtoken.verify(req.headers.authorization.split(' ')[1], 'schoolcom',function(err, decode){
+      if(err){
+        console.log(err);
+        req.user = undefined;
+      }
+      req.user = decode;
+      next();
+    });
+  } else {
+    req.user = undefined;
+    next();
+  };
+});
 var routes = require('./api/routes/todoListRoutes');
 routes(app);
 
